09bcd3c49be972207256c186d529cd82cd798420
miklukha <miklukha.anna@ukr.net>
2024-12-29 16:16:03 +0100

    deleted unnecessary files

--- app/src/main/java/com/example/firstlab/services/CalculatorService.kt
+++
@@ -1,7 +0,0 @@
-package com.example.firstlab.services
-
-class CalculatorService {
-    fun sumValues(value1: Int, value2: Int) = value1 + value2
-
-    fun multiplyValues(value1: Int, value2: Int) = value1 * value2
-}
